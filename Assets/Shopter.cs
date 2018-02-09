9abc789871f7bf3635be4ebbfe80f6e07e728b7e
khoai1234 <anhkhoaivn@gmail.com>
2018-02-09 15:27:08 +0700

    first commit

---
+++ Assets/Shopter.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+class Shopter : MonoBehaviour
+{
+    public Rigidbody prefab;
+    public float speed = 10.0f;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var obj = (Rigidbody)Instantiate(this.prefab, transform.position, transform.rotation);
+            obj.velocity = (transform.forward + transform.up / 2) * speed;
+        }
+
+    }
+
+}
